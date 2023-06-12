771afbfbb9ba2bd1e3fce9b1720b245939aa6288
RicardoVessaro <ricardolvessaro@gmail.com>
2023-06-12 20:56:13 -0300

    Section 3 - 37 FizzBuzz Project - Coding - Test Part 1

--- 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class FizzBuzzTest {
 
     // If number is divisible by 3, print Fizz
-    // If number is divisible by 5, print Buzz
     // If number is divisible by 3 and 5, print FizzBuzz
     // If number is NOT divisible by 3 or 5, then print the number
 
@@ -16,7 +15,22 @@ class FizzBuzzTest {
     @Test
     @Order(1)
     void testForDivisibleByThree() {
-        fail("fail"); // Used to fail a test (good for new tests).
+//        fail("fail"); // Used to fail a test (good for new tests).
+
+        String expected = "Fizz";
+
+        assertEquals(expected, FizzBuzz.compute(3), "Should return Fizz");
     }
 
+    // If number is divisible by 5, print Buzz
+    @DisplayName("Divisible by Five")
+    @Test
+    @Order(2)
+    void testForDivisibleByFive() {
+//        fail("fail"); // Used to fail a test (good for new tests).
+
+        String expected = "Buzz";
+
+        assertEquals(expected, FizzBuzz.compute(5), "Should return Buzz");
+    }
 }

2462c9e0e404667361a84670081fa0d7cfbf5aee
RicardoVessaro <ricardolvessaro@gmail.com>
2023-08-14 20:47:19 -0300

    55 - Mocking with Mockito - Coding - Part 3

--- 2.20-starting-project/src/test/java/com/luv2code/test/MockAnnotationTest.java
+++ 2.20-starting-project/src/test/java/com/luv2code/test/MockAnnotationTest.java
@@ -6,11 +6,16 @@ import com.luv2code.component.models.CollegeStudent;
 import com.luv2code.component.models.StudentGrades;
 import com.luv2code.component.service.ApplicationService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes= MvcTestingExampleApplication.class)
 public class MockAnnotationTest {
@@ -43,6 +48,23 @@ public class MockAnnotationTest {
         studentOne.setLastname("Roby");
         studentOne.setEmailAddress("eric.roby@luv2code_school.com");
         studentOne.setStudentGrades(studentGrades);
+    }
+
+    @DisplayName("When & Verify")
+    @Test
+    public void assertEqualsTestAddGrades() {
+        when(applicationDao.addGradeResultsForSingleClass(
+               studentGrades.getMathGradeResults()
+        )).thenReturn(100.00);
+
+        assertEquals(100, applicationService.addGradeResultsForSingleClass(
+                studentOne.getStudentGrades().getMathGradeResults()));
+
+        verify(applicationDao)
+                .addGradeResultsForSingleClass(studentGrades.getMathGradeResults());
+
+        verify(applicationDao, times(1))
+                .addGradeResultsForSingleClass(studentGrades.getMathGradeResults());
     }
 
 }

7ceefbbcc3825f293135b5361bc86714d9039b6d
RicardoVessaro <ricardolvessaro@gmail.com>
2023-08-16 21:23:35 -0300

    64 - Reflection Test Utils - Coding - Part 3

--- 2.20-starting-project/src/test/java/com/luv2code/test/ReflectionTestUtilsTest.java
+++ 2.20-starting-project/src/test/java/com/luv2code/test/ReflectionTestUtilsTest.java
@@ -4,6 +4,7 @@ import com.luv2code.component.MvcTestingExampleApplication;
 import com.luv2code.component.models.CollegeStudent;
 import com.luv2code.component.models.StudentGrades;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +12,8 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes= MvcTestingExampleApplication.class)
 public class ReflectionTestUtilsTest {
@@ -36,5 +39,17 @@ public class ReflectionTestUtilsTest {
                 new StudentGrades(new ArrayList<>(Arrays.asList(
                         100.0, 85.0, 76.50, 91.75
                 ))));
+    }
+
+    @Test
+    public void getPrivateField() {
+        assertEquals(1, ReflectionTestUtils.getField(studentOne, "id"));
+    }
+
+    @Test
+    public void invokePrivateMethod() {
+        assertEquals("Eric 1",
+                ReflectionTestUtils.invokeMethod(studentOne, "getFirstNameAndId"),
+                "Fail private method not call");
     }
 }
